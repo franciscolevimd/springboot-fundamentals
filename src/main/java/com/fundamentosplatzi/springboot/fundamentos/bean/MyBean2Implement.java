1329bbc08564fe1e5e3c12be2638f5ed9498f9d1
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-31 11:24:33 -0600

    Implementation with other bean.

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBean2Implement.java
@@ -0,0 +1,8 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBean2Implement implements MyBean {
+    @Override
+    public void print() {
+        System.out.println("Other hellof from my bean.");
+    }
+}
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/MyConfigurationBean.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/configuration/MyConfigurationBean.java
@@ -1,6 +1,7 @@
 package com.fundamentosplatzi.springboot.fundamentos.configuration;
 
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean2Implement;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanImplement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 public class MyConfigurationBean {
     @Bean
     public MyBean beanOperation() {
-       return new MyBeanImplement();
+       return new MyBean2Implement();
     }
 }
