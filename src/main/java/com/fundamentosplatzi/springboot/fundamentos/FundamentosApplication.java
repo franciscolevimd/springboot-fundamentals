38eaebe7df33ac2f8bfd15ea20fc2d6380daa263
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-31 21:26:47 -0600

    Added log.

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,7 @@ import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentosplatzi.springboot.fundamentos.pojo.UserPojo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class FundamentosApplication implements CommandLineRunner {
 	@Autowired
 	@Qualifier("componentTwoImplement")
@@ -34,7 +36,14 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDependency.greet();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
-		System.out.println(myBeanWithProperties.function());
-		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword() + "-" + userPojo.getAge());
+		log.info(myBeanWithProperties.function());
+		log.info(userPojo.getEmail() + "-" + userPojo.getPassword() + "-" + userPojo.getAge());
+		try {
+			// Error
+			int value = 10/0;
+			log.debug("Mi valor: " + value);
+		} catch (Exception e) {
+			log.error("Esto es un error al dividir entre cero " + e.getMessage());
+		}
 	}
 }
--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,16 +1,20 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Slf4j
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
     @Autowired
     MyOperation myOperation;
 
     @Override
     public void printWithDependency() {
+        log.info("Hemos ingresado al método printWithDependency");
         int number = 15;
+        log.debug("EL número enviado como parámetro a la dependencia operation es: " + number);
         myOperation.add(number);
-        System.out.println(myOperation.add(number));
-        System.out.println("Hello from a bean implementation.");
+        log.info(String.valueOf(myOperation.add(number)));
+        log.info("Hello from a bean implementation.");
     }
 }
